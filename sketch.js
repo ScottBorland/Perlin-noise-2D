037d293eda07b3530c5d00314fb91ec8ea976b6b
ScottBorland <scottborland0@gmail.com>
2019-10-30 21:38:02 +0000

    perlin noise flow field completed

--- sketch.js
+++ sketch.js
@@ -1,33 +1,75 @@
 //var xoff1 = 0;
 //var xoff2 = 1000;
-var inc = 0.01;
+var inc = 0.1;
 var start = 0;
 
+var scl = 10;
+var cols, rows;
+
+var zoff = 0;
+
+var fr;
+
+var particles = [];
+
+var flowfield;
+
 function setup() {
+	background(255);
 	createCanvas(200, 200);
 	pixelDensity(1);
+	cols = floor(width / scl);
+	rows = floor(height / scl);
+	fr = createP('');
+
+	flowfield = new Array(cols * rows);
+
+	for(var i = 0; i < 400; i++){
+	particles[i] = new Particle();
+	}
 }
 
 function draw() {
-	var yoff = 0;
 
-	loadPixels();
-	for(var y = 0; y < height; y++){
+//background(255);
+var yoff = 0;
+
+	for(var y = 0; y < rows; y++){
 		var xoff = 0;
-	for(var x = 0; x < width; x++){
-			var index = (x + y * width) * 4;
-			var r = noise(xoff, yoff) * 255;
-			pixels[index + 0] = r;
-			pixels[index + 1] = r;
-			pixels[index + 2] = r;
-			pixels[index + 3] = 255;
-
+	for(var x = 0; x < cols; x++){
+			var index = x + y * cols;
+			var angle = noise(xoff, yoff, zoff) * TWO_PI * 4;
+			var v = p5.Vector.fromAngle(angle);
+			v.setMag(1);
+			flowfield[index] = v;
 			xoff += inc;
+			// stroke(0, 50);
+			// strokeWeight(1);
+			// push();
+			// translate(x*scl, y*scl);
+			// rotate(v.heading());
+			// line(0, 0, scl, 0);
+			// pop();
 		}
 		yoff += inc;
+
+		zoff += 0.001;
 	}
-	updatePixels();
+
+	for(var i = 0; i < particles.length; i++){
+		particles[i].follow(flowfield);
+		particles[i].update();
+		particles[i].show();
+		particles[i].edges();
+	}
+
+	fr.html(floor(frameRate()));
 }
+
+
+
+
+
 /* cool static effect
 for(var x = 0; x < width; x++){
 	for(var y = 0; y < height; y++){
